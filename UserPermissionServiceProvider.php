badc26a06625ac81472d36609666c742dff303c5
brahim-ewd <brahim.ewd@gmail.com>
2024-10-03 16:26:12 +0100

    start

---
+++ UserPermissionServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace YourVendor\YourPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class YourPackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register config file
+        $this->mergeConfigFrom(__DIR__.'/config/userpermission.php', 'userpermission');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publish the configuration file
+        $this->publishes([
+            __DIR__.'/config/userpermission.php' => config_path('userpermission.php'),
+        ], 'userpermission');
+
+        // Load migrations
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+    }
+}
